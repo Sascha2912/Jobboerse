89ff345115c61984d99f2ac531304c87d22dd4ca
Sascha Holzigel <S.Holzigel@gmx.de>
2024-02-12 22:55:38 +0100

    Migrations implementiert
    Job, User, Job_user, Category, Company

--- Jobboerse-Webapp/database/migrations/2024_02_12_151630_create_jobs_table.php
+++ Jobboerse-Webapp/database/migrations/2024_02_12_151630_create_jobs_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('jobs', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedInteger('salary');
+            $table->foreignId('category_id')->constrained('categories');
+            $table->foreignId('company_id')->constrained('companies');
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('jobs');
     }
 };
