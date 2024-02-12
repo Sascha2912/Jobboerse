85c960f3792f4917c7c2cd00f02c9f967f39c495
Sascha Holzigel <S.Holzigel@gmx.de>
2024-02-12 22:56:27 +0100

    Routes implementiert
    Job, User, Job_User, Category, Company

--- Jobboerse-Webapp/routes/web.php
+++ Jobboerse-Webapp/routes/web.php
@@ -16,3 +16,42 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Definieren von CRUD-Operationen (Create, Read, Update, Delete) 
+
+// Jobs
+Route::get('/jobs', 'JobController@index');
+Route::get('/jobs/create', 'JobController@create');
+Route::post('/jobs', 'JobController@store');
+Route::get('/jobs/{job}', 'JobController@show');
+Route::get('/jobs/{job}/edit', 'JobController@edit');
+Route::put('/jobs/{job}', 'JobController@update');
+Route::delete('/jobs/{job}', 'JobController@destroy');
+
+
+// Companies
+Route::get('/companies', 'CompanyController@index');
+Route::get('/companies/create', 'CompanyController@create');
+Route::post('/companies', 'CompanyController@store');
+Route::get('/companies/{company}', 'CompanyController@show');
+Route::get('/companies/{company}/edit', 'CompanyController@edit');
+Route::put('/companies/{company}', 'CompanyController@update');
+Route::delete('/companies/{company}', 'CompanyController@destroy');
+
+// Categories
+Route::get('/categories', 'CategoryController@index');
+Route::get('/categories/create', 'CategoryController@create');
+Route::post('/categories', 'CategoryController@store');
+Route::get('/categories/{category}', 'CategoryController@show');
+Route::get('/categories/{category}/edit', 'CategoryController@edit');
+Route::put('/categories/{category}', 'CategoryController@update');
+Route::delete('/categories/{category}', 'CategoryController@destroy');
+
+// Users
+Route::get('/users', 'UserController@index');
+Route::get('users/create', 'UserController@create');
+Route::post('users', 'UserController@store');
+Route::get('/users/{user}', 'UserController@show');
+Route::get('/users/{user}/edit', 'UserController@edit');
+Route::put('/users/{user}', 'UserController@update');
+Route::delete('/users/{user}', 'UserController@destroy');
