a00727c54ab567e222b3ca8d2319e759401fb535
Sascha Holzigel <S.Holzigel@gmx.de>
2024-02-18 22:06:22 +0100

    Prototyp Ansicht Category, Company, User, Job, Job_User, Index und Impressum

---
+++ Jobboerse-Webapp/resources/views/impressum/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+    <article class="container">
+        <h1>Impressum</h1>
+        <section>
+            <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+            <p><span class="col-md-auto">Max Mustermann</span>
+                <span class="col-md-auto">Musterladen (Einzelunternehmer)</span>
+                <span class="col-md-auto">Musterstra&szlig;e 111</span>
+                <span class="col-md-auto">Geb&auml;ude 44</span>
+                <span class="col-md-auto">90210 Musterstadt</span></p>
+        </section>
+        <section>
+            <h3>Kontakt</h3>
+            <p><span class="col-md-auto">Telefon: +49 (0) 123 44 55 66</span>
+                <span class="col-md-auto">Telefax: +49 (0) 123 44 55 99</span>
+                <span class="col-md-auto">E-Mail: mustermann@musterfirma.de</span></p>
+        </section>
+        <section>
+            <h3>Umsatzsteuer-ID</h3>
+            <p><span class="col-md-auto">Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; &sect; 27 a Umsatzsteuergesetz:</span>
+                <span class="col-md-auto">DE999999999</span></p>
+        </section>
+        <section>
+            <h3>EU-Streitschlichtung</h3>
+            <p><span class="col-md-auto">Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a href="https://ec.europa.eu/consumers/odr/" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr/</a>.</span>
+                <span class="col-md-auto">Unsere E-Mail-Adresse finden Sie oben im Impressum.</span></p>
+        </section>
+        <section>
+            <h3>Verbraucher&shy;streit&shy;beilegung/<br>
+                Universal&shy;schlichtungs&shy;stelle</h3>
+            <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+        </section>
+        <p>Quelle: <a href="https://www.e-recht24.de">e-recht24.de</a></p>
+    </article>
+@endsection
