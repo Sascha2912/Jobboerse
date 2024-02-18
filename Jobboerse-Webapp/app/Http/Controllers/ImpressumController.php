9b45a4690944c60c7ac1ea68e83bb1bef4eeb9d1
Sascha Holzigel <S.Holzigel@gmx.de>
2024-02-18 22:50:15 +0100

    Routes neu definiert.

---
+++ Jobboerse-Webapp/app/Http/Controllers/ImpressumController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Contracts\Container\BindingResolutionException;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
+
+class ImpressumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        return view('impressum.index');
+
+    }
+
+}
--- Jobboerse-Webapp/routes/web.php
+++ Jobboerse-Webapp/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\JobUserController;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ImpressumController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,41 +27,28 @@ Route::get('/', function () {
 // Definieren von CRUD-Operationen (Create, Read, Update, Delete)
 
 // Jobs
-Route::get('/jobs', 'JobController@index');
-Route::get('/jobs/create', 'JobController@create');
-Route::post('/jobs', 'JobController@store');
-Route::get('/jobs/{job}', 'JobController@show');
-Route::get('/jobs/{job}/edit', 'JobController@edit');
-Route::put('/jobs/{job}', 'JobController@update');
-Route::delete('/jobs/{job}', 'JobController@destroy');
-
+Route::prefix('jobs')->group(function (){
+    Route::get('/', [JobController::class, 'index'])->name('jobs.index');
+    Route::get('/create', [JobController::class, 'create'])->name('jobs.create');
+    Route::post('/', [JobController::class, 'store'])->name('jobs.store');
+    Route::get('/{job}', [JobController::class, 'show'])->name('jobs.show');
+    Route::get('/{job}/edit', [JobController::class, 'edit'])->name('jobs.edit');
+    Route::put('/{job}', [JobController::class, 'update'])->name('jobs.update');
+    Route::delete('/{job}', [JobController::class, 'destroy'])->name('jobs.destroy');
+});
 
 // Companies
-Route::get('/companies', 'CompanyController@index');
-Route::get('/companies/create', 'CompanyController@create');
-Route::post('/companies', 'CompanyController@store');
-Route::get('/companies/{company}', 'CompanyController@show');
-Route::get('/companies/{company}/edit', 'CompanyController@edit');
-Route::put('/companies/{company}', 'CompanyController@update');
-Route::delete('/companies/{company}', 'CompanyController@destroy');
+Route::prefix('companies')->group(function (){
+    Route::get('/', [CompanyController::class, 'index'])->name('companies.index');
+    Route::get('/create', [CompanyController::class, 'create'])->nmae('companies.create');
+    Route::post('/', [CompanyController::class, 'store'])->name('companies.store');
+    Route::get('/{company}', [CompanyController::class, 'show'])->name('companies.show');
+    Route::get('/{company}/edit', [CompanyController::class, 'edit'])->name('companies.edit');
+    Route::put('/{company}', [CompanyController::class, 'update'])->name('companies.update');
+    Route::delete('/{company}', [CompanyController::class, 'destroy'])->nmae('companies.destroy');
+});
 
 // Categories
-//Route::get('categories', [CategoryController::class, 'index']);
-//Route::get('/categories/create', [CategoryController::class, 'create']);
-//Route::post('/categories', [CategoryController::class, 'store']);
-//Route::get('/categories/{category}', [CategoryController::class, 'show']);
-//Route::get('/categories/{category}/edit', [CategoryController::class, 'edit']);
-//Route::put('/categories/{category}', [CategoryController::class, 'update']);
-//Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
-
-// Route::get('/categories', 'CategoryController@index')->name('categories.index');
-// Route::get('/categories/create', 'CategoryController@create')->name('categories.create');
-// Route::post('/categories', 'CategoryController@store')->name('categories.store');
-// Route::get('/categories/{category}', 'CategoryController@show')->name('categories.show');
-// Route::get('/categories/{category}/edit', 'CategoryController@edit')->name('categories.edit');
-// Route::put('/categories/{category}', 'CategoryController@update')->name('categories.update');
-// Route::delete('/categories/{category}', 'CategoryController@destroy')->name('categories.destroy');
-
 Route::prefix('categories')->group(function (){
     Route::get('/', [CategoryController::class, 'index'])->name('categories.index');
     Route::get('/create', [CategoryController::class, 'create'])->name('categories.create');
@@ -66,42 +59,30 @@ Route::prefix('categories')->group(function (){
     Route::delete('/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
 });
 
-//Route::prefix('categories')->group(function (){
-//    Route::get('/', 'CategoryController@index')->name('categories.index');
-//    Route::get('/create', 'CategoryController@create')->name('categories.create');
-//    Route::post('/', 'CategoryController@store')->name('categories.store');
-//    Route::get('/{category}', 'CategoryController@show')->name('categories.show');
-//    Route::get('/{category}/edit', 'CategoryController@edit')->name('categories.edit');
-//    Route::put('/{category}', 'CategoryController@update')->name('categories.update');
-//    Route::delete('/{category}', 'CategoryController@destroy')->name('categories.destroy');
-//});
-
-//Route::controller(CategoryController::class)->group(function (){
-//    Route::get('/', 'CategoryController@index')->name('categories.index');
-//    Route::get('/create', 'CategoryController@create')->name('categories.create');
-//    Route::post('/', 'CategoryController@store')->name('categories.store');
-//    Route::get('/{category}', 'CategoryController@show')->name('categories.show');
-//    Route::get('/{category}/edit', 'CategoryController@edit')->name('categories.edit');
-//    Route::put('/{category}', 'CategoryController@update')->name('categories.update');
-//    Route::delete('/{category}', 'CategoryController@destroy')->name('categories.destroy');
-//});
-
-
-
 // Users
-Route::get('/users', 'UserController@index');
-Route::get('users/create', 'UserController@create');
-Route::post('users', 'UserController@store');
-Route::get('/users/{user}', 'UserController@show');
-Route::get('/users/{user}/edit', 'UserController@edit');
-Route::put('/users/{user}', 'UserController@update');
-Route::delete('/users/{user}', 'UserController@destroy');
+Route::prefix('users')->group(function (){
+    Route::get('/', [UserController::class, 'index'])->name('user.index');
+    Route::get('/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/', [UserController::class, 'store'])->name('users.store');
+    Route::get('/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::get('/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+});
 
 // Job_Users
-Route::get('/job_users', 'JobUserController@index');
-Route::get('job_users/create', 'JobUserController@create');
-Route::post('job_users', 'JobUserController@store');
-Route::get('/job_users/{job_user}', 'JobUserController@show');
-Route::get('/job_users/{job_user}/edit', 'JobUserController@edit');
-Route::put('/job_users/{job_user}', 'JobUserController@update');
-Route::delete('/job_users/{job_user}', 'JobUserController@destroy');
+Route::prefix('job_users')->group(function (){
+    Route::get('/', [JobUserController::class, 'index'])->name('job_users.index');
+    Route::get('/create', [JobUserController::class, 'create'])->name('job_users.create');
+    Route::post('/', [JobUserController::class, 'store'])->name('job_users.store');
+    Route::get('/{job_user}', [JobUserController::class, 'show'])->name('job_users.show');
+    Route::get('/{job_user}/edit', [JobUserController::class, 'edit'])->name('job_users.edit');
+    Route::put('/{job_user}', [JobUserController::class, 'update'])->name('job_users.update');
+    Route::delete('/{job_user}', [JobUserController::class, 'destroy'])->name('job_users.destroy');
+});
+
+// Index
+Route::get('/', [IndexController::class, 'index'])->name('index.index');
+
+// Impressum
+Route::get('/impressum', [ImpressumController::class, 'index'])->name('impressum.index');
